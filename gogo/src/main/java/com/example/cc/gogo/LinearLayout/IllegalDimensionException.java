99eb062b0c4cc2d8d9203e4c7098cfb81f517170
poi233 <593657039@qq.com>
2016-11-06 12:19:16 +0800

    add LinearLayout...

---
+++ gogo/src/main/java/com/example/cc/gogo/LinearLayout/IllegalDimensionException.java
@@ -0,0 +1,15 @@
+package com.example.cc.gogo.LinearLayout;
+
+public class IllegalDimensionException extends Exception {
+
+	public IllegalDimensionException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalDimensionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
